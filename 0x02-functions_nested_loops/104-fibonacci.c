1bfbd99a3a7fab61570bb87d7b6482a84764a55c
bankydeee <bankydee2@gmail.com>
2023-09-13 23:12:36 -0700

    fibo4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,21 +5,20 @@
 */
 int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2;
-	unsigned long sum;
-	unsigned long fib3;
+    unsigned long fib1 = 1, fib2 = 2;
+    unsigned long fib3;
 
-	printf("%lu, %lu", fib1, fib2);
+    printf("%lu, %lu", fib1, fib2);
 
-	for (int count = 3; count <= 98; count++)
-	{
-		fib3 = fib1 + fib2;
-		printf(", %lu", fib3);
+    for (int count = 3; count <= 98; count++)
+    {
+        fib3 = fib1 + fib2;
+        printf(", %lu", fib3);
 
-		fib1 = fib2;
-		fib2 = fib3;
-	}
+        fib1 = fib2;
+        fib2 = fib3;
+    }
 
-	printf("\n");
-	return (0);
+    printf("\n");
+    return (0);
 }
