c01d8353dbc6e9045c04bdbd32a998d64c916507
bankydeee <bankydee2@gmail.com>
2023-09-18 20:21:28 -0700

    put c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - procedure title
+ * @str: input
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[1] != '\0'; i++)
+_putchar (str[i]);
+_putchar ('\n');
+}
